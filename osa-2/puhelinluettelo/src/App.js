88ebec21360ac64290278d0935f79c175961fe81
Taneli Hongisto <taneli.hongisto@protonmail.com>
2020-09-02 12:41:18 +0300

    Osa 2 valmis

--- osa-2/puhelinluettelo/src/App.js
+++ osa-2/puhelinluettelo/src/App.js
@@ -1,5 +1,34 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import personService from './services/persons'
 
+
+const Notice = ({ message }) => {
+  if (message !== "" && message !== null && typeof message !== "undefined" ) {
+    return (
+      <div className="notice">
+        {message}
+      </div>
+    )
+  }
+
+  else {
+    return null
+  }
+}
+
+const Error = ({ message }) => {
+  if (message !== "" && message !== null && typeof message !== "undefined" ) {
+    return (
+      <div className="error">
+        {message}
+      </div>
+    )
+  }
+
+  else {
+    return null
+  }
+}
 
 const FilterForm = ({ keyword, keywordSetter }) => {
   return (
@@ -47,48 +76,107 @@ const NewPersonForm = (
   )
 }
 
-const FilteredPersons = ({ list, keyword }) => {
+const FilteredPersons = ({ list, keyword, deletePerson }) => {
   const filtered = list.filter(a => a.name.includes(keyword))
 
   return (
-    filtered.map(a => <div key={a.name}>{a.name} {a.number}</div>)
+    filtered.map(a => (
+      <div key={a.id}>
+        {a.name} {a.number}
+        <button onClick={() => deletePerson(a)}>delete</button>
+      </div>
+    ))
   )
 }
 
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1231244'},
-    { name: 'Aro Hellas', number: '040-1237244'},
-    { name: 'Ato Hlas', number: '040-7231254'},
-    { name: 'Art Hella', number: '040-1731244'},
-    { name: 'Arto Hlaas', number: '040-1261244'},
-    { name: 'Arto Helas', number: '040-1221244'},
-  ]) 
+  const [ persons, setPersons ] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ keyword, setKeyword ] = useState('')
+  const [ noticeMessage, setNoticeMessage ] = useState(null)
+  const [ errorMessage, setErrorMessage ] = useState(null)
+
+
+  useEffect(() => {
+    const doesntExist = {
+      id: -1,
+      name: "doesn't exist",
+      number: ""
+    }
+
+    personService
+      .getAll()
+      .then(response => {
+        // setPersons(response)
+        setPersons(response.concat(doesntExist))
+      })
+  }, [])
 
   const addPerson = (e) => {
     e.preventDefault()
+    const personIndex = persons.map(a => a.name).indexOf(newName)
 
-    if (persons.map(a => a.name).indexOf(newName) !== -1) {
-      alert(`${newName} already in phonebook!`)
-      setNewName('')
-      setNewNumber('')
+    if  (
+          personIndex !== -1 && 
+          window.confirm(`${newName} is already added. Update number?`)
+        ) {
+      personService
+        .update(persons[personIndex].id, newNumber)
+        .then(response => {
+          const newPersons = (persons
+              .filter(a => a.id !== response.id)
+              .concat(response))
+          setPersons(newPersons)
+          setNoticeMessage(`Updated ${newName}`)
+          setTimeout(() => {setNoticeMessage(null)}, 3000)
+        })
     }
 
     else {
-      const newPerson = {name: newName, number: newNumber}
-      setPersons(persons.concat(newPerson))
-      setNewName('')
-      setNewNumber('')
+      const newPerson = { name: newName, number: newNumber }
+      personService
+        .create(newPerson)
+        .then(response => {
+          setPersons(persons.concat(response))
+          setNoticeMessage(`Added ${newName}`)
+          setTimeout(() => {setNoticeMessage(null)}, 3000)
+        })
+    }
+
+    setNewName('')
+    setNewNumber('')
+  }
+
+  const deletePerson = (person) => {
+    if (window.confirm(`Really delete ${person.name}?`)) {
+      personService
+        .remove(person.id)
+        .then(response => {
+          if (response === 1) {
+            setPersons(persons.filter(a => a.id !== person.id))
+            setErrorMessage(`${person.name} has already been removed from server`)
+            setTimeout(() => {setErrorMessage(null)}, 3000)
+          }
+          else {
+            setPersons(persons.filter(a => a.id !== response.id))
+            setNoticeMessage(`Removed ${person.name}`)
+            setTimeout(() => {setNoticeMessage(null)}, 3000)
+          }
+        })
     }
   }
 
   return (
     <div>
       <h1>Phonebook</h1>
-      <FilterForm keyword={keyword} keywordSetter={setKeyword} />
+      <FilterForm
+        keyword={keyword}
+        keywordSetter={setKeyword}
+      />
+
+      <Notice message={noticeMessage} />
+      <Error message={errorMessage} />
 
       <h2>Add new</h2>
       <NewPersonForm
@@ -100,7 +188,11 @@ const App = () => {
       />
 
       <h2>Numbers</h2>
-      <FilteredPersons list={persons} keyword={keyword} />
+      <FilteredPersons
+        list={persons}
+        keyword={keyword}
+        deletePerson={deletePerson}
+      />
     </div>
   )
 }
---
+++ osa-2/puhelinluettelo/src/services/persons.js
@@ -0,0 +1,61 @@
+import axios from 'axios'
+
+
+const baseUrl = 'http://localhost:3001/persons'
+
+
+const getAll = () => {
+  return (
+    axios
+      .get(baseUrl)
+      .then(response => response.data)
+  )
+}
+
+const create = (newObject) => {
+  return (
+    axios
+      .post(baseUrl, newObject)
+      .then(response => response.data)
+  )
+}
+
+const remove = (id) => {
+  const remObject = axios
+    .get(`${baseUrl}/${id}`)
+    .then(response => response.data)
+    .catch(() => {
+      console.log(`tried remove, but no object on id ${id}`)
+      return 1
+    })
+
+  remObject.then(response => {
+    if (response !== 1) {
+      axios.delete(`${baseUrl}/${id}`)
+      console.log(`succesfully removed id ${id}`)
+    }
+  })
+
+  return remObject
+}
+
+const update = (id, newNumber) => {
+  console.log(`updating id ${id}`)
+
+  return getAll()
+    .then(response => {
+      const oldObject = response.find(a => a.id === id)
+      console.log(oldObject)
+
+      const updatedObject = { ...oldObject, number: newNumber}
+      console.log(updatedObject)
+
+      axios
+        .put(`${baseUrl}/${id}`, updatedObject)
+
+      return updatedObject
+    })
+}
+
+
+export default { getAll, create, remove, update }
